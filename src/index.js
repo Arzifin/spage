1d7397418a117c607f66cb462f71102353d42a92
Arzi <noobspin@hotmail.com>
2023-01-30 23:52:54 +0200

    Updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 // Strictmode disabled due to possible limitations
+/*
 ReactDOM.render(
-  /* <React.StrictMode> */
+  <React.StrictMode/>,
   <App />,
-  document.getElementById('root')
+  document.getElementById('root');
+);
+*/
+
+root.render(
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode> 
 );
